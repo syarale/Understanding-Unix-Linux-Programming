cfb105606a42086aa0a563034738eaff62d655f6
syarale <syinlei2023@gmail.com>
2024-09-23 21:47:04 +0800

    feature: add who (v1.0)
    Signed-off-by: syarale <syinlei2023@gmail.com>

---
+++ chapter2/who.c
@@ -0,0 +1,47 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <utmp.h>
+
+#define SHOW_HOST 0
+
+void show_info(struct utmp* utmp_ptr) {
+  if (utmp_ptr->ut_type != USER_PROCESS) { /* users only */
+    return;
+  }
+
+  printf("% -8.8s", utmp_ptr->ut_name);
+  printf(" ");
+  printf("% -8.8s", utmp_ptr->ut_line);
+  printf(" ");
+  printf("% 10d", utmp_ptr->ut_time);
+  printf(" ");
+
+#ifdef SHOW_HOST
+  printf("(%s)", utmp_ptr->ut_host);
+#endif
+
+  printf("\n");
+  return;
+}
+
+int main() {
+  int utmp_fd;
+  struct utmp utmp_buf;
+
+  ssize_t nbytes = sizeof(utmp_buf);
+
+  if ((utmp_fd = open(UTMP_FILE, O_RDONLY)) == -1) {
+    perror(UTMP_FILE);
+    exit(1);
+  }
+
+  while (read(utmp_fd, &utmp_buf, nbytes) == nbytes) {
+    show_info(&utmp_buf);
+  }
+
+  close(utmp_fd);
+
+  return 0;
+}
